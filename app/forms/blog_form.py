54f22f4a8938477e2b9ae46029d999b00e4932f0
Matt <matthewdavid99@comcast.net>
2023-05-29 14:49:23 -0400

    add owner.id in blog_form.py

--- app/forms/blog_form.py
+++ app/forms/blog_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
 from app.models import Blog
 
@@ -7,4 +7,5 @@ class BlogForm(FlaskForm):
 
     blog_title = StringField('Title', validators=[DataRequired(), Length(min=1, max=50, message='Blog title must be between 1 and 50 characters')])
     blog_name = StringField('Name', validators=[DataRequired(), Length(min=1, max=50, message='Blog Name must be between 1 and 50 characters')])
+    owner_id = IntegerField('owner_id', validators=[DataRequired()])
     submit = SubmitField('Create')
