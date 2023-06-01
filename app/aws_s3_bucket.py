9f3406f1a6f5a91edd8b274f4a4804e0318bd1f2
Matt <matthewdavid99@comcast.net>
2023-05-31 21:51:57 -0400

    added aws functionality in post_routes.py, also created new file aws_s3_bucket.py, also everyone needs to change their .env file to contain the AWS_KEY and the AWS_SECRET_KEY

---
+++ app/aws_s3_bucket.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name='us-east-2'
+)
+
+bucket = 'flaskbrrr'
