f8eab99756894cbcb28fe871b85e705ba8694a96
Matt <matthewdavid99@comcast.net>
2023-06-04 12:20:51 -0400

    users redux is done

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import blogsReducer from './blogs'
+import userReducer from './users';
 
 const rootReducer = combineReducers({
   session,
-  blogs: blogsReducer
+  blogs: blogsReducer,
+  user: userReducer
 });
 
 
