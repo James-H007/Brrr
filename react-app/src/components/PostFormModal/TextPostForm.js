3d35940fe794404c84311b806abe8941ed2700da
sterling_hebert <sterling_hebert@outlook.com>
2023-06-06 18:08:49 -0500

    added validator errors to the text post type

--- react-app/src/components/PostFormModal/TextPostForm.js
+++ react-app/src/components/PostFormModal/TextPostForm.js
@@ -1,39 +1,83 @@
-
-import "./PostFormModal.css"
+import "./PostFormModal.css";
+import React, { useState } from "react";
 
 const TextPostForm = ({ postType }) => {
-    //Should receive a prop of what the post type is based on the click
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [textError, setTextError] = useState("");
 
-    return (
-        <div className="post-form-container">
-            <div className="post-form-content">
-                <header className="post-form-header">
-                    <img src="https://thelifeofyourtime.files.wordpress.com/2016/05/bloodroot.jpg" alt="flower" className="post-maker-icon" />
-                    Username
-                </header>
-                <form className="post-form">
-                    <input
-                        type="text"
-                        className="post-form-input-title"
-                        placeholder="Title..."
-                        name="title"
-                    />
-                    <textarea
-                        type="textarea"
-                        className="post-form-input-text"
-                        placeholder="Your post here..."
-                        name="text"
-                    />
-                    <div className="close-post-buttons">
-                        <button className="poster-button">Close</button>
-                        <button className="poster-button">Post</button>
-                    </div>
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
-                </form>
-            </div>
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        </div>
-    )
-}
+    if (title.length < 1 || title.length > 225) {
+      setTitleError("Title should be between 1 and 225 characters.");
+      return;
+    }
 
-export default TextPostForm
+    if (text.length < 1 || text.length > 1200) {
+      setTextError("Text should be between 1 and 1200 characters.");
+      return;
+    }
+
+    // Perform form submission or further processing here
+    // send the data to a backend server
+
+    // Reset the form after submission
+    setTitle("");
+    setText("");
+  };
+
+  return (
+    <div className="post-form-container">
+      <div className="post-form-content">
+        <header className="post-form-header">
+          <img
+            src="https://thelifeofyourtime.files.wordpress.com/2016/05/bloodroot.jpg"
+            alt="flower"
+            className="post-maker-icon"
+          />
+          Username
+        </header>
+        <form className="post-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="post-form-input-title"
+            placeholder="Title..."
+            name="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
+          {titleError && <div className="errors">{titleError}</div>}
+          <textarea
+            className="post-form-input-text"
+            placeholder="Your post here..."
+            name="text"
+            value={text}
+            onChange={handleTextChange}
+          />
+          {textError && <div className="errors">{textError}</div>}
+          <div className="close-post-buttons">
+            <button className="poster-button">Close</button>
+            <button
+              className="poster-button"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Post
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TextPostForm;
