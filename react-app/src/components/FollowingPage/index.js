ff9a03faf0ec1fc50e8eeb212246642d3fa8f131
Matt <matthewdavid99@comcast.net>
2023-06-05 20:05:00 -0400

    gave you the data

--- react-app/src/components/FollowingPage/index.js
+++ react-app/src/components/FollowingPage/index.js
@@ -1,7 +1,80 @@
-
 import "./FollowingPage.css"
+import { getCurrentUser } from "../../store/users"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+
 
 const FollowingPage = () => {
+
+    const dispatch = useDispatch()
+    const currentUser = useSelector(state => state.user.currentUser)
+
+    useEffect(() => {
+        dispatch(getCurrentUser())
+
+        /*
+        currentUser (marnie@aa.io) ===
+            {
+                blogs: [
+                {
+                bannerImgUrl: 'djsb--------------------kds.png',
+                blogAvatarUrl: 'djsbbkbkjbkbkjkdvd.png',
+                blogName: 'Marnie',
+                blogTitle: 'TESTTSTSTSTSTSTSTSTSTSTS____________________',
+                createdAt: 'Wed, 31 May 2023 22:49:58 GMT',
+                defaultBlog: false,
+                description: 'vjsdkdvsddsv',
+                followerCount: 1,
+                id: 2,
+                ownerId: 2,
+                posts: [
+                  {
+                    blogId: 2,
+                    comments: [],
+                    createdAt: 'Thu, 01 Jun 2023 02:28:32 GMT',
+                    id: 4,
+                    imageEmbedCode: 'svbjskbsvdjsv.png',
+                    likes: 0,
+                    notes: null,
+                    postDescription: 'ldskndsvklsdvdvs',
+                    postImages: [],
+                    postTitle: 'Test',
+                    postType: 'image',
+                    reblogs: null,
+                    updatedAt: 'Thu, 01 Jun 2023 02:28:32 GMT',
+                    userId: 2,
+                    videoEmbedCode: 'jdskbvkjdvsbsdvj'
+                  }
+                ],
+                updatedAt: 'Thu, 01 Jun 2023 22:59:15 GMT'
+              }, {
+                bannerImgUrl: 'test.png',
+                blogAvatarUrl: 'test2.png',
+                blogName: 'TESTING',
+                blogTitle: 'TEST',
+                createdAt: 'Thu, 01 Jun 2023 22:20:39 GMT',
+                defaultBlog: true,
+                description: 'dvnsvlddsnvklsd',
+                followerCount: 0,
+                id: 5,
+                ownerId: 2,
+                posts: [],
+                updatedAt: 'Thu, 01 Jun 2023 22:20:39 GMT'
+              },
+
+            ],
+            createdAt: 'Wed, 31 May 2023 22:49:58 GMT',
+            email: 'marnie@aa.io',
+            firstName: 'Marnie',
+            id: 2,
+            lastName: 'Jones',
+            profilePicUrl: 'https://64.media.tumblr.com/d713916b3661f9cae54f9f880168a2f2/tumblr_ny5vbt2nLA1umv52oo2_500.png',
+            updatedAt: 'Wed, 31 May 2023 22:49:58 GMT',
+            username: 'marnie'
+        }
+        */
+
+    }, [dispatch])
 
 
 
